da4c55da4d42fc9e86a43ef5ea9bf13a5ec57423
Erwan <vaillant.erwan49@gmail.com>
2024-05-22 14:11:08 +0200

    debut site

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, redirect, g, render_template, session, jsonify
-
+import genetique as gen
+import recuit_simule as recuit
+import tabou as tabou
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    return render_template('index.html',range_4=range(4), range_10= range(10))
 
 if __name__ == '__main__':
     app.run(debug=True)
